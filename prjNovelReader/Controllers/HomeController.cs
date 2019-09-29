52a7250ba4721aeffd6d7b07aa7aa1f9167df1ae
BANA <hhhgodhhh@gmail.com>
2019-09-30 00:37:25 +0800

    字形與大小選擇移至前端以JS控制

--- prjNovelReader/Controllers/HomeController.cs
+++ prjNovelReader/Controllers/HomeController.cs
@@ -37,72 +37,13 @@ namespace prjNovelReader.Controllers
             return View(homeService.ShowChapterC(novelId));
         }
 
-        public ActionResult ReadChapter(int id,int? glyph, int? size)
+        public ActionResult ReadChapter(int id,int glyph=0, int size=0)
         {
-            if (glyph == null)
-            {
-                glyph = 0;
-            }
-            if (size == null)
-            {
-                size = 0;
-            }
             ViewData["Id"] = id;
             ViewData["PreviousId"] = (id-1);
             ViewData["NextId"] = (id+1);
             ViewData["currentGlyph"] = glyph;
             ViewData["currentSize"] = size;
-            string[] Size = { "小", "中", "大" };
-            string[] Glyph = {"標楷體","微軟正黑體" };
-            var dict = new Dictionary<string, string>();
-            for(var i = 0; i < 3; i++)
-            {
-                dict.Add(Size[i],i.ToString());
-            }
-            ViewData["sizeDList"] = homeService.GetDropdownList(
-                "size",
-                dict,
-                new { @class = "form-control", id = "size" },
-                size.ToString(),
-                false,
-                "");
-
-            dict.Clear();
-            for(var i = 0; i < 2; i++)
-            {
-                dict.Add(Glyph[i], i.ToString());
-            }
-            ViewData["glyphDList"] = homeService.GetDropdownList(
-                "glyph",
-                dict,
-                new { @class = "form-control", id = "glyph" },
-                glyph.ToString(),
-                false,
-                ""
-                );
-            switch (size)
-            {
-                case 0:
-                    ViewBag.size = "16px;";
-                    break;
-                case 1:
-                    ViewBag.size = "24px;";
-                    break;
-                case 2:
-                    ViewBag.size = "32px;";
-                    break;
-            }
-            switch (glyph)
-            {
-                case 0:
-                    ViewBag.glyph = "DFKai-sb;";
-                    break;
-                case 1:
-                    ViewBag.glyph = "Microsoft JhengHei;";
-                    break;
-            }
-            //ViewData["chapterText"]=homeService.ReadChapter(id);
-
             return View(homeService.ReadChapter(id));
         }
 
